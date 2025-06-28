97a50f268d943bcd85f980822bc6eddfa8adb864
tofu212 <parveen123456@gmail.com>
2025-06-28 13:01:03 +0800

    lesson19

---
+++ lesson19/demo.js
@@ -0,0 +1,19 @@
+console.log("hello hi bye bye")
+document.getElementByID('Aaryavratid')
+
+var c = {shout: console.log("shouting"), age: 14, gender: "male"}
+var x = "How to claculate"
+e.innerHTML = "Baic addition, subtraction, division, and multiplication"
+
+var a = 10
+var b = 28
+
+var sum = a + b
+console.log("the sum is", sum)
+
+ var diff = b - a
+ console.log("the diff is", diff)
+ 
+ var div = 80 / 2
+ Console.log("the div is", 40)
+
