274667ae5200f0dcc902435dcd2d4e3c3dce3d2a
tofu212 <parveen123456@gmail.com>
2025-06-29 12:52:20 +0800

    lesson20

---
+++ 20lesson/demo.js
@@ -0,0 +1,24 @@
+function askMaamToDrinkWater(){
+    console.log("Maam may I drink water")
+    console.log("Maam I wont go wash room")
+    console.log("Maam I am very thirsty")
+}
+askMaamToDrinkWater() 
+askMaamToDrinkWater()
+askMaamToDrinkWater()
+askMaamToDrinkWater()
+askMaamToDrinkWater()
+askMaamToDrinkWater()
+
+function addNumbers(a, b, c){
+    var sum = a + b + c
+    console.log("the sum of a, b, c is", sum)
+}
+addNumbers(74, 92, 22)
+
+function multiplyNumbers(x, y, z){
+    var result = x * y * z
+    return result
+}
+var k = multiplyNumbers(21, 33, 9)
+console.log("the multiplication of result of x, y,  and z is", k )
