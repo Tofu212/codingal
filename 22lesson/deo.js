a4af878f94ccf922182638e2933d27d679bfdc09
tofu212 <parveen123456@gmail.com>
2025-07-06 12:51:05 +0800

    lesson21

---
+++ 22lesson/deo.js
@@ -0,0 +1,12 @@
+var a = 5
+var b = "Banana"
+
+var X = {favfruit: function () {console.log("My favourite food is Banana")}, tellFavFood: 'His favourite food is apple', maths: function(a){return a+a+a+a+a}}
+
+c.favfruit()
+console.log(c.tellFavFood)
+console.log(c.maths(10))
+
+console.log(b.toLowerCase())
+
+console.log(math.random())
