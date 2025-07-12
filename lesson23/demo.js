b52368449cbfaea9a1551ab3f21a6a08cb53b21d
tofu212 <parveen123456@gmail.com>
2025-07-12 12:53:49 +0800

    lesson23

---
+++ lesson23/demo.js
@@ -0,0 +1,23 @@
+var a = [32, 64, 72, 77, 09]
+
+console.log(a[0])
+console.log(a[3])
+console.log(a.length)
+
+var b = ['apple', 'banana', 'guava', 'pear', 'cherry', 'watermelon']
+
+console.log(b.join("."))
+console.log(b[1])
+console.log(b[0])
+console.log(b.pop())
+console.log(b)
+
+function sum(a, b c){
+    return a+b+c
+}
+function average(a, b, c){
+    return sum(a, b, c)/3
+}
+
+var result = average(10, 10, 10)
+console.log(result)
