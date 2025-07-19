5f02edc3e93346c326645b814e4153410862b963
tofu212 <parveen123456@gmail.com>
2025-07-19 12:49:29 +0800

    25lesson

--- 25lesson/demo.js
+++ 25lesson/demo.js
@@ -1,5 +1,5 @@
 var a = 20
-var b = 'Toyo'
+var b = 'Tokyo'
 var c = true
 var d = false
 var x = {greeting: "Namaste, my name is Aaryavrat", number: 999, homeworkDone: true, sendData: function(){return 8*24}}
