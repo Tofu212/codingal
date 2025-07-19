882a9448f27efe89318d87a06bfba6279a21b54f
tofu212 <parveen123456@gmail.com>
2025-07-19 12:42:52 +0800

    25lesson

---
+++ 25lesson/demo.js
@@ -0,0 +1,27 @@
+var a = 20
+var b = 'Toyo'
+var c = true
+var d = false
+var x = {greeting: "Namaste, my name is Aaryavrat", number: 999, homeworkDone: true, sendData: function(){return 8*24}}
+console.log(x.greeting)
+console.log(x.number)
+console.log(x.homeworkDone)
+console.log(x.sendData())
+
+a = 100
+const y = 200
+let z = 300
+z = 150
+//y = 250
+
+i = 0
+while(i<100){
+    console.log("Ha")
+    i=i+1
+}
+
+i = 0
+while(i<100){
+console.log(Math.random())
+i=i+1
+}
