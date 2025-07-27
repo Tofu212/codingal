7d30430fd00527de04540ca2106a5cc0e0ad47b4
tofu212 <parveen123456@gmail.com>
2025-07-27 12:57:11 +0800

    lesson28

---
+++ 28lesson/demo.js
@@ -0,0 +1,40 @@
+var a = 213
+var b = 'Pizza'
+var c = true
+
+console.log(typeof(a))
+
+console.log(typeof(b))
+
+console.log(typeof(c))
+
+var d = String(a)
+console.log(typeof(d))
+
+var e = '777'
+console.log(typeof(Number(f)))
+
+var f = {name: 'Aaryavrat', age: 14, city: Singapore}
+console.log(typeof(f))  
+
+var g = [12, 42, 45, 14, 98, 58]
+console.log(typeof(g))
+console.log(typeof(NaN))
+
+var h = new Date()
+console.log(h)
+console.log(typeof(h))
+
+var i = null
+console.log(typeof(null))
+
+console.log(typeof(j))
+
+try{
+    console.log(z)
+}catch(err){
+    console.log(err)
+}finally{
+    console.log('hahahhahhah')
+}
+    console.log('code is about to ending')
