3acdd1c132a3f83a2fffdab23c43a1ea0c102791
tofu212 <parveen123456@gmail.com>
2025-08-31 13:02:20 +0800

    38lesson

--- 37 project/demo.py
+++ 37 project/demo.py
@@ -8,3 +8,8 @@ a = 84
 b = True
 c = False
 
+if a < 20:
+    print("a is less than 20")
+else:
+    print("a is greater than 20")
+
---
+++ 38project/demo.py
@@ -0,0 +1,55 @@
+password = input("Enter your password:")
+maths = input("what is 2 + 2:")
+a = 293492
+print(type(maths))
+
+b = 3.24
+print(type(b))
+
+c = True
+d = False
+print(type(c))
+print(type(d))
+
+e = 'hello, world'
+print(type(e))
+
+a = str(a)
+print(type(a))
+
+c = int(c)
+d = int(d)
+
+print(type(c))
+print(type(d))
+
+print(c)
+print(d)
+
+z = 10
+z = float(z)
+print(type(z))
+print(z)
+#can convert string to integer only if string contains digits
+
+print(16/5)#division
+print(16//5)#floor division
+print(16%5)#modulus (remainder)
+print(4**3)#exponentiation
+
+w = 100000
+v = 200000
+print(w > v)#greater than
+print(w < v)#smaller than
+print(w == v)#equal to
+print(w != v)#smaller than or equal to
+
+j = w  * v + 20 / -4 +6 ** 2 // 234
+print(j)
+
+print(len(e))
+print(e[0])
+print(e[0:4])#slicing end(4) is not inclusive
+
+print(e + e)
+print(e * 4)
