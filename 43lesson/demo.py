1f94782da2623e60d156a2332ba2f40fd72d0ad1
tofu212 <parveen123456@gmail.com>
2025-09-20 12:57:47 +0800

    43lesson

---
+++ 43lesson/demo.py
@@ -0,0 +1,29 @@
+items_in_your_house = ['knife', 63, 'pizza', 'chocolate', 3000, True, False, 'orange', 'car']
+
+
+print(items_in_your_house)
+print(type(items_in_your_house))
+
+print(items_in_your_house[3])
+print(len(items_in_your_house))\
+
+items_in_your_house.append('Clother')
+items_in_your_house.append(62)
+items_in_your_house.append('Bicycle')
+
+print(items_in_your_house)
+
+items_in_your_house.pop()
+
+print(items_in_your_house)
+
+#print(item_in_your_house.sort())
+
+nl = [75, 453, 345, 354, 56, 1, 0, 44, 746]
+nl.sort()
+print(nl)
+
+nl.reverse()
+print(nl)
+
+
