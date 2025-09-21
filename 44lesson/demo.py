f564afd3f92c0e38af80784cf5884e031b9b8bf6
tofu212 <parveen123456@gmail.com>
2025-09-21 13:01:04 +0800

    lsson44

---
+++ 44lesson/demo.py
@@ -0,0 +1,22 @@
+my_dict = {'name': 'Evin and Aaryavrat', 'age': 30, 'lucky_numbers': [3, 42, 13, 57, 1, 5], 'country': 'India', 'blood': 'no blood'}
+
+print(my_dict['age'])
+print(my_dict['lucky_numbers'][2])
+my_dict['age'] = 50
+
+print(my_dict)
+
+my_dict['fruits'] = ['apple', 'banana']
+
+print(my_dict)
+
+my_dict.pop('age')
+print(my_dict)
+
+
+hahahihihu = (34, 'sun', 'moon', 934, [23, 234, 2345], {'name': 'Aaryavrat', 'age': 40})
+
+print(hahahihihu)
+
+# hahahihihu.append(32)
+hahahihihu[1] = 'jupiter'
